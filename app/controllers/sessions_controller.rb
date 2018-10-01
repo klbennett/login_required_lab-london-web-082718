b9180021aca3da077ea4a9a8d4ffb6ff35a924b0
Kay B <kaylouisebennett@gmail.com>
2018-10-01 22:14:49 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
 def create
   if params[:name] && !params[:name].empty?
     session[:name] = params[:name]
-    redirect_to secret_path
+    redirect_to secrets_path
   else
     redirect_to new_session_path
   end
