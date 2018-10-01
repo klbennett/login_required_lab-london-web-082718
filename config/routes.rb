0642640d366780896688d09e293038d801245b15
Kay B <kaylouisebennett@gmail.com>
2018-10-01 22:25:18 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -58,7 +58,6 @@ root 'sessions#new'
 
 get '/sessions/new', to: 'sessions#new'
 get '/sessions/show', to: 'sessions#show'
-  post '/login' => 'sessions#create'
 get '/login', to: 'sessions#new', as: 'login'
 post '/sessions/create', to: 'sessions#create'
 post '/sessons/destroy', to: 'sessions#destroy'
